b468148350f71ae7a95c1dc9077c13d4cae08fb3
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-02-28 00:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def y1(x):
+    return x**3 - 0.5 * x**2 + x + 1
 
 
-# Press the green button in the gutter to run the script.
+def y2(x):
+    return math.log(x + 1) + 1
+
+
+def y3(x):
+    return (1 / 100) * math.exp(x) * (math.cos(100 * x) + 100)
+
+
+def y4(x):
+    return math.exp(x)
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
